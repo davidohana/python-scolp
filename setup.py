08eee08266452d900301812a8a947e7cc86f281c
davidoh <david.ohana@ibm.com>
2019-06-05 17:18:55 +0300

    fixed package manifest

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="scolp",
-    version="0.1.1",
+    version="0.1.2",
     author="David Ohana",
     author_email="davidoha@gmail.com",
     description="Streaming Column Printer",
@@ -13,8 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/davidohana/python-scolp",
     packages=setuptools.find_packages(),
+    py_modules=['scolp'],
     classifiers=[
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries",
